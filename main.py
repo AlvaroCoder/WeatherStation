61123ee72597eda4e64cadfadd3877b72915ea6b
camilavaleria4 <camilavaleria4@hotmail.com>
2023-04-08 18:02:08 -0500

    templates

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, templates
 
 app=FastAPI()
 
 @app.get("/")
 async def root():
     return {"message":"Welcome"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
