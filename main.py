9d0a4e5260949871a6be84544473e625dfb502e2
camilavaleria4 <camilavaleria4@hotmail.com>
2023-04-08 20:25:48 -0500

    incompleto rutas

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI, Request, templates
+from fastapi import FastAPI 
+""" , Request, templates """
 
 app=FastAPI()
 
 @app.get("/")
 async def root():
     return {"message":"Welcome"}
+
+""" @app.get("/")
+async def login(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request}) """
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
