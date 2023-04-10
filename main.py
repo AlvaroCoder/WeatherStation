66ff5e91877020d34cc3caaf676ddd6b4f0d3c80
camilavaleria4 <camilavaleria4@hotmail.com>
2023-04-09 21:08:22 -0500

    ROUTERS LISTAS

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from fastapi import FastAPI 
-""" , Request, templates """
+from fastapi import FastAPI, APIRouter,status
+from routers import login
 
 app=FastAPI()
+
+#routers
+app.include_router(login.router)
+
+router=APIRouter(prefix="/home",
+                tags=["Home page"],
+                responses={status.HTTP_404_NOT_FOUND:{"message":"Page not found"}})
 
 @app.get("/")
 async def root():
     return {"message":"Welcome"}
 
-@app.get("/login")
+
+""" @app.get("/login")
 async def login(request: Request):
     return templates.TemplateResponse("index.html", {"request": request}) 
-
+"""
