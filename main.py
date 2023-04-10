fe8777536e0ae2a24d801726048d21b4f57bb459
camilavaleria4 <camilavaleria4@hotmail.com>
2023-04-09 20:43:43 -0500

    Intento rutas

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-from fastapi import FastAPI
-from routers import login
+from fastapi import FastAPI 
 """ , Request, templates """
 
 app=FastAPI()
-app.include_router(router=login.router)
+
 @app.get("/")
 async def root():
     return {"message":"Welcome"}
 
-""" @app.get("/")
+@app.get("/login")
 async def login(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request}) """
+    return templates.TemplateResponse("index.html", {"request": request}) 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
